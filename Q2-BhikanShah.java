da1b67484afec4906d7754799c529cb3a80e5f2e
BhikanShah <73951567+BhikanShah@users.noreply.github.com>
2021-08-26 06:29:55 -0700

    Add files via upload

---
+++ Q2-BhikanShah.java
@@ -0,0 +1,39 @@
+import java.util.*;
+ 
+public class GFG
+{
+    public String longestCommonPrefix(String[] a)
+    {
+        int size = a.length;
+ 
+        
+        if (size == 0)
+            return "";
+ 
+        if (size == 1)
+            return a[0];
+ 
+       
+        Arrays.sort(a);
+ 
+        
+        int end = Math.min(a[0].length(), a[size-1].length());
+ 
+        
+        int i = 0;
+        while (i < end && a[0].charAt(i) == a[size-1].charAt(i) )
+            i++;
+ 
+        String pre = a[0].substring(0, i);
+        return pre;
+    }
+ 
+    
+    public static void main(String[] args)
+    {
+        GFG gfg = new GFG();
+        String[] input = {"geegfghgg", "geeks", "geek", "geezer"};
+        System.out.println( "The longest Common Prefix is : " +
+                                   gfg.longestCommonPrefix(input));
+    }
+}
